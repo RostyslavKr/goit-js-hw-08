1c50095867b3e71328c5cbbaa3b792d0ca80e929
RostyslavKr <106476898+RostyslavKr@users.noreply.github.com>
2022-12-14 16:58:42 +0200

    corect package.json

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,6 @@
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 
 // Change code below this line
@@ -11,33 +11,30 @@ galleryContainer.insertAdjacentHTML('beforeend', listGallery);
 galleryContainer.addEventListener('click', onGalleryContainerClick);
 
 let gallery = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionsDelay: 250,
-    captionPosition: 'bottom',
-    overlayOpacity: 0.8,
-    scrollZoom: false,
+  captionsData: 'alt',
+  captionsDelay: 250,
+  captionPosition: 'bottom',
+  overlayOpacity: 0.8,
+  scrollZoom: false,
 });
 
 gallery.on('show.simplelightbox');
 function createGalleryItems(gallery) {
-    return gallery.map(({ preview, original, description }) => {
-        return `
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
          <a class="gallery__item" href="${original}">
          <img class="gallery__image" src="${preview}" alt="${description}" />
          </a>`;
-        })
-        .join('');
+    })
+    .join('');
 }
-
 
 function onGalleryContainerClick(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const isGalleryImage = e.target.classList.contains('gallery__image');
-    if (!isGalleryImage) {
-        return;
-    }
-    
+  const isGalleryImage = e.target.classList.contains('gallery__image');
+  if (!isGalleryImage) {
+    return;
+  }
 }
-
-
