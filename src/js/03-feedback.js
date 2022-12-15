4e155328f0fce05ad46ea7fecaa735e15f592a56
RostyslavKr <106476898+RostyslavKr@users.noreply.github.com>
2022-12-15 14:26:24 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,17 +5,33 @@ const formData = {};
 
 const refs = {
   form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input'),
+  message: document.querySelector('.feedback-form textarea'),
 };
 
 populateMessageOutput();
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.email.addEventListener('input', throttle(onEmailInput, 500));
+refs.message.addEventListener('input', throttle(onMessageInput, 500));
 
 function onFormInput(e) {
   formData[e.target.name] = e.target.value;
   console.log((formData[e.target.name] = e.target.value));
   localStorage.setItem(USER_KEY, JSON.stringify(formData));
+}
+
+function onEmailInput(evt) {
+  const email = JSON.stringify(formData[evt.target.name]);
+
+  localStorage.setItem('Email', email);
+}
+
+function onMessageInput(evt) {
+  const message = JSON.stringify(formData[evt.target.name]);
+
+  localStorage.setItem(USER_KEY, message);
 }
 
 function onFormSubmit(e) {
@@ -29,12 +45,16 @@ function getSaveInputs(key) {
 }
 
 function populateMessageOutput() {
-  const formData = getSaveInputs(USER_KEY);
-  if (!formData) {
-    return;
+  const savedMessage = localStorage.getItem(USER_KEY);
+  const savedEmail = localStorage.getItem('Email');
+
+  if (savedMessage) {
+    refs.message.value = JSON.parse(savedMessage);
+    formData.message = refs.message.value;
   }
 
-  for (const key in formData) {
-    refs.form.elements[key].value = formData[key];
+  if (savedEmail) {
+    refs.email.value = JSON.parse(savedEmail);
+    formData.email = refs.email.value;
   }
 }
